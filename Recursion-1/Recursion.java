dc2ea851dfe346e5647bc7a5d531931372148c3d
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-08 22:55:06 +0530

    count 7 & 8

--- Recursion-1/Recursion.java
+++ Recursion-1/Recursion.java
@@ -6,6 +6,8 @@ public class Recursion {
     System.out.println(bunnyEars(10));
     System.out.println(triangle(5));
     System.out.println(sumDigits(123));
+    System.out.println(count7(172737));
+    System.out.println(count8(182884));  
   }
 
   public static int factorial(int n){
@@ -27,5 +29,18 @@ public class Recursion {
   public static int sumDigits(int n){
     if(n==0 || n==1) return n;
     return n%10 + sumDigits(n/10);
+  }
+
+  public static int count7(int n){
+    if(n==0) return 0;
+    if(n%10==7) return 1+count7(n/10);
+    return count7(n/10);
+  }
+
+  public static int count8(int n){
+    if(n==0) return 0;
+    if(n%100 == 88) return 2+count8(n/10);
+    if(n%10==8) return 1+count8(n/10);
+    return count8(n/10);
   }
 }
