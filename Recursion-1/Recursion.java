9972b87f0a0423b0ccdcd1b6aba6af5741ae7611
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-08 18:07:40 +0530

    Triangle + SumDigits

--- Recursion-1/Recursion.java
+++ Recursion-1/Recursion.java
@@ -4,6 +4,8 @@ public class Recursion {
   public static void main(String[] args) {
     System.out.println(factorial(5));
     System.out.println(bunnyEars(10));
+    System.out.println(triangle(5));
+    System.out.println(sumDigits(123));
   }
 
   public static int factorial(int n){
@@ -15,5 +17,15 @@ public class Recursion {
     if(n==0) return 0;
     if(n%2==0) return 3 + bunnyEars(n-1);
     return 2 + bunnyEars(n-1);
+  }
+
+  public static int triangle(int rows){
+    if(rows==0) return 0;
+    return rows + triangle(rows-1);
+  }
+
+  public static int sumDigits(int n){
+    if(n==0 || n==1) return n;
+    return n%10 + sumDigits(n/10);
   }
 }
