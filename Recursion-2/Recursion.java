c60775cfd181a1425ba54f320708d5c571a8e2ed
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-10 22:38:59 +0530

    group sum with 6

--- Recursion-2/Recursion.java
+++ Recursion-2/Recursion.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 public class Recursion {
   public static void main(String[] args){
-    int a[] = {1,6,2,11,3};
+    int a[] = {1,4,6,11,3};
     System.out.println(groupSum(0, a, 12));
+    System.out.println(groupSum6(0, a, 14));
   }
 
   public static boolean groupSum(int start, int[] nums, int target) {
     if (start >= nums.length) return (target == 0);
     if (groupSum(start + 1, nums, target - nums[start])) return true;
     if (groupSum(start + 1, nums, target)) return true;
+    return false;
+  }
+
+  public static boolean groupSum6(int start, int[] nums, int target) {
+    if(start==nums.length)  return (target==0);
+    if(groupSum6(start+1,nums,target-nums[start]))  return true;
+    if(nums[start]!=6)
+      if(groupSum6(start+1,nums,target))  return true;
     return false;
   }
 }
