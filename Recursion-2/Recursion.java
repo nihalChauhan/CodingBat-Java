ccef76ba944ecd01cbc7fa6101780a834c257bef
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-10 23:08:08 +0530

    group sum no adj

--- Recursion-2/Recursion.java
+++ Recursion-2/Recursion.java
@@ -5,6 +5,7 @@ public class Recursion {
     int a[] = {1,4,6,11,3};
     System.out.println(groupSum(0, a, 12));
     System.out.println(groupSum6(0, a, 14));
+    System.out.println(groupNoAdj(0, a, 7));
   }
 
   public static boolean groupSum(int start, int[] nums, int target) {
@@ -19,6 +20,13 @@ public class Recursion {
     if(groupSum6(start+1,nums,target-nums[start]))  return true;
     if(nums[start]!=6)
       if(groupSum6(start+1,nums,target))  return true;
+    return false;
+  }
+
+  public static boolean groupNoAdj(int start, int[] nums, int target) {
+    if (start >= nums.length) return (target == 0);
+    if (groupNoAdj(start + 2, nums, target - nums[start])) return true;
+    if (groupNoAdj(start + 1, nums, target)) return true;
     return false;
   }
 }
