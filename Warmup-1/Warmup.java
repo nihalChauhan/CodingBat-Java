b056706324cfef49ff6d6087e29e91c9192f68a6
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-29 23:54:59 +0530

    wmp-1 c2

--- Warmup-1/Warmup.java
+++ Warmup-1/Warmup.java
@@ -3,11 +3,44 @@ import java.util.*;
 public class Warmup {
   public static void main(String[] args){
     System.out.println(sleepIn(false, false));
+    System.out.println(monkeyTrouble(false, false));
+    System.out.println(sumDouble(2,2));
+    System.out.println(diff21(34));
+    System.out.println(parrotTrouble(true,8));
+    System.out.println(makes10(2,8));
   }
 
   public static boolean sleepIn(boolean weekday, boolean vacation) {
     if(!weekday || vacation)
       return true;
       return false;
+  }
+
+  public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    return !(aSmile ^ bSmile);
+  }
+
+  public static int sumDouble(int a, int b) {
+    if(a==b)
+      return a*4;
+    return a+b;
+  }
+
+  public static int diff21(int n) {
+    if(n > 21)
+      return 2*(n-21);
+    return 21-n;
+  }
+
+  public static boolean parrotTrouble(boolean talking, int hour) {
+    if(talking && (hour<7 || hour>20))
+      return true;
+    return false;
+  }
+
+  public static boolean makes10(int a, int b) {
+    if(a==10 || b==10 || a+b==10)
+      return true;
+    return false;
   }
 }
